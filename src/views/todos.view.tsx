690838edae117b4abce9ba6ad12f031e2555639f
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2025-01-15 17:56:10 +0500

    Написал обертку для страницы todo листа

--- src/views/todos.view.tsx
+++ src/views/todos.view.tsx
@@ -1,5 +1,12 @@
 const TodosView: React.FC = (): React.ReactElement => {
-  return <></>;
+  return (
+    <div className="w-full max-w-6xl min-h-svh mx-auto px-2 pt-7">
+      <div className="w-full min-h-svh grid grid-cols-3 gap-4">
+        <div className="col-span-2"></div>
+        <div className="col-start-3"></div>
+      </div>
+    </div>
+  );
 };
 
 export default TodosView;
