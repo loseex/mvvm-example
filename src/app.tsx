b99f8cb8276c9b799048afa4d096076c901924a3
Дмитрий Чехонадских <112265709+loseex@users.noreply.github.com>
2025-01-15 17:44:23 +0500

    Фикс прошлого коммита

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import { lazy, Suspense } from "react";
+import { Preloader } from "./components/ui/preloader";
 
 const TodosView = lazy(() => import("@/views/todos.view"));
 
 export const App: React.FC = (): React.ReactElement => {
   return (
-    <Suspense>
+    <Suspense fallback={<Preloader />}>
       <TodosView />
     </Suspense>
   );
